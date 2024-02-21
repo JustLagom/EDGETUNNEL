11eb594904a3888d9d8af4e54a8c43c9b633fce7
JustLagom <93416015+JustLagom@users.noreply.github.com>
2024-02-22 01:16:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,13 +3,13 @@ import { connect } from 'cloudflare:sockets';
 
 let userID = 'bfda82c3-3630-4ca0-8a57-63ce835dd1da';
 
-let proxyIP = '';// 反代IP
+let proxyIP = '';
 
-let sub = '';// 订阅生成器
+let sub = 'sub.cmliussss.workers.dev';// 订阅生成器
 
-let subconverter = '';// 订阅转换后端
+let subconverter = 'proxysub.justlagom.workers.dev';// 订阅转换后端
 
-let subconfig = "";// 订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/cmliu/edgetunnel/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini";// 订阅配置文件
 
 let socks5Address = '';// Example:  user:pass@host:port  or  host:port
 
