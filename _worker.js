e7c067519ddecdbbc0682669b32dabcfc7568078
JustLagom <93416015+JustLagom@users.noreply.github.com>
2024-03-08 02:41:56 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -797,9 +797,10 @@ function socks5AddressParser(address) {
 function revertFakeInfo(content, userID, hostName, isBase64) {
 	if (isBase64) content = atob(content);//Base64解码
 	content = content.replace(new RegExp(fakeUserID, 'g'), userID).replace(new RegExp(fakeHostName, 'g'), hostName);
-	if (isBase64) content = btoa(content);//Base64编码
-
-	return content;
+        if (isBase64) {
+                content = btoa(content)
+        }
+        return content;
 }
 
 /**
