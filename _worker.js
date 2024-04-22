e253450eb8ad34ff4dc09eb403077ca1583a0fe4
JustLagom <93416015+JustLagom@users.noreply.github.com>
2024-04-22 12:48:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -590,7 +590,7 @@ async function handleDNSQuery(udpChunk, webSocket, vlessResponseHeader, log) {
 	// no matter which DNS server client send, we alwasy use hard code one.
 	// beacsue someof DNS server is not support DNS over TCP
 	try {
-		const dnsServer = '8.8.4.4'; // change to 1.1.1.1 after cf fix connect own ip bug
+		const dnsServer = '8.8.8.8'; // change to 1.1.1.1 after cf fix connect own ip bug
 		const dnsPort = 53;
 		/** @type {ArrayBuffer | null} */
 		let vlessHeader = vlessResponseHeader;
